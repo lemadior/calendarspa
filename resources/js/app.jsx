3f672a436a4aa5bc8d435f09949f907fffe1bf94
Yurii Miroshnyk <lemadioro@gmail.com>
2024-08-23 10:05:07 +0300

    Add Inertia to the project

---
+++ resources/js/app.jsx
@@ -0,0 +1,21 @@
+import './bootstrap';
+// import '../css/app.css'; // Необходимо для подключения Tailwind
+
+import { createInertiaApp } from '@inertiajs/react';
+import { createRoot } from 'react-dom/client';
+//import Layout from '@/Layouts/Layout'; // На начальном этапе это нужно закомментировать, если не нужны Лейауты
+
+createInertiaApp({
+    title: title => `LAREACT | ${title}`, // Это нужно для динимечского изменения титла через компонент Head. Если не нужно - можно отключить.
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true });
+        const page = pages[ `./Pages/${name}.jsx` ];
+
+        // page.default.layout = page.default.layout || (page => <Layout children={ page } />); // Это тоже можно отключить, если не нужны Лейауты.
+
+        return page;
+    },
+    setup ({ el, App, props }) {
+        createRoot(el).render(<App { ...props } />);
+    },
+});
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Inertia\Inertia;
+
 Route::get('/', function () {
-
-    return view('welcome');
+    return Inertia::render('Home');
 });
-
 
 
 // Authentication pages
